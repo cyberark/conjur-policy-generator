aba9a07e455956c8b07af0eaaf0a8805520d4024
Ryan Prior <ryanprior@gmail.com>
2017-11-07 13:04:40 -0600

    Adds CLI script to generate MAMLs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
+const parseArgs = require('minimist')
 const mom = require('./mom')
 const humans = mom.humans
 
-console.log(humans(30, 30, 6))
+const parseOptions = {
+  alias: {
+    u: 'users',
+    g: 'groups',
+    k: 'usersPerGroup'
+  },
+  default: {
+    users: 2,
+    groups: 1,
+    usersPerGroup: 1
+  }
+}
+
+const {users, groups, usersPerGroup} = parseArgs(process.argv.slice(2), parseOptions)
+
+console.log(humans(users, groups, usersPerGroup))
