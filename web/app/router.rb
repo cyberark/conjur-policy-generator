b1a2e5534201c11eac50607288e65b3f94e37e1f
Ryan Prior <ryanprior@gmail.com>
2018-08-20 19:10:48 -0500

    Updates routes and index.html for GH pages setup

--- web/app/router.rb
+++ web/app/router.rb
@@ -1,8 +1,13 @@
 class Router
   include Inesita::Router
 
+  def go_home
+    lambda { go_to '/conjur-policy-generator/' }
+  end
+
   def routes
-    route '/', to: Home
-    route '/about', to: About
+    route '/conjur-policy-generator/about', to: About
+    route ('/conjur-policy-generator/?'), to: Home
+    route '/', to: Home, on_enter: go_home
   end
 end
