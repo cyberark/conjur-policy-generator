ea54ff5d0520f390970264502a1de893f766ab25
Ryan Prior <ryanprior@gmail.com>
2018-08-29 15:55:34 -0500

    Removes unneeded div wrapper

--- web/app/components/home.rb
+++ web/app/components/home.rb
@@ -2,17 +2,15 @@ class Home
   include Inesita::Component
 
   def render
-    div do
-      case store.current_generator
-      when 'secret_control'
-        component SecretControlPolicy
-      when 'secrets'
-        component SecretsPolicy
-      when 'humans'
-        component HumansPolicy
-      else
-        text "Error: can't show the policy (#{store.current_generator}, #{store.current_generator.class})"
-      end
+    case store.current_generator
+    when 'secret_control'
+      component SecretControlPolicy
+    when 'secrets'
+      component SecretsPolicy
+    when 'humans'
+      component HumansPolicy
+    else
+      text "Error: can't show the policy (#{store.current_generator}, #{store.current_generator.class})"
     end
   end
 end
